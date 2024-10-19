32ef7fba44d797ff7fd5224626d7cd1d105b968c
AlyaMell <alina0203181@gmail.com>
2024-10-19 17:52:45 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,10 @@
+number_of_homework = 12
+number_of_hours = 1.5
+name_of_course = 'Python'
+one_task = number_of_hours / number_of_homework
+
+
+print(number_of_homework)
+print(number_of_hours)
+print(one_task)
+print(name_of_course , number_of_homework , number_of_hours , one_task )
