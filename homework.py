d8e9e94d4e203e44a3bc26c9e3a09e5d1ef99a23
AlyaMell <alina0203181@gmail.com>
2024-11-22 18:25:02 +0300

    Add files via upload

--- homework.py
+++ homework.py
@@ -3,8 +3,7 @@ number_of_hours = 1.5
 name_of_course = 'Python'
 one_task = number_of_hours / number_of_homework
 
+print('Курс:', name_of_course + ',', 'Всего задач:' + str(number_of_homework) + ',', 'Затрачено часов:',
+      str(number_of_hours)+',','Среднее время выполнения:', one_task , 'часа.')
 
-print(number_of_homework)
-print(number_of_hours)
-print(one_task)
-print(name_of_course , number_of_homework , number_of_hours , one_task )
+# Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
